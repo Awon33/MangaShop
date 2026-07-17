9cb6451a0a87d279801e6eaf59318c6b52540086
Awon Abbas <awon4585@gmail.com>
2026-07-18 00:32:18 +0500

    Prepare project for PythonAnywhere deployment

--- cart/models.py
+++ cart/models.py
@@ -19,6 +19,15 @@ class Cart(models.Model):
     def total_items(self):
         return sum(item.quantity for item in self.items.all())
 
+    def get_total_price(self):
+        return self.total_price
+
+    def get_tax(self):
+        return 0.0
+
+    def get_final_total(self):
+        return self.total_price
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
